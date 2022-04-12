8d92a6391f33d306a3e5719f8b6b44000681a447
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-12 10:05:55 -0400

    db initialized

---
+++ db/models/users.js
@@ -0,0 +1,17 @@
+const pool = require('../pool')
+
+const createUser = async ({ username, password }) => {
+  const {
+    rows: [user],
+  } = await pool.query(
+    `
+        INSERT INTO users(username, password)
+        VALUES ($1, $2)
+        RETURNING *
+    `,
+    [username, password]
+  )
+  return user
+}
+
+module.exports = { createUser }
