fadc1fde3aad7df698020a8be1ee31f4e86bf715
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-12 20:19:42 -0400

    cors

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const { COOKIE_SECRET } = require('./secrets')
 
 const path = require('path')
 
+const cors = require('cors')
+
 const { authRequired } = require('./api/utils')
 
 require('dotenv').config()
 
 const app = express()
 
+app.use(cors())
 app.use(morgan('dev'))
 
 app.use(cookieParser(COOKIE_SECRET))
@@ -26,9 +29,8 @@ app.get('/test', authRequired, (req, res, next) => {
   res.send('You are authorized')
 })
 
-app.get(
-  '*'
-  // express.static(path.join(__dirname, './client', 'build', 'index.html'))
-)
+app.get('*', (req, res, next) => {
+  res.status(404).send("Sorry we can't find that page..")
+})
 
 module.exports = app
