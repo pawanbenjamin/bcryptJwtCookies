4aa82824435b2aae13ce85472fd25e4c3c64ce51
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-13 10:47:48 -0400

    register

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -2,23 +2,26 @@ import { useState } from 'react'
 import { register } from './api/auth'
 
 const Register = () => {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState(null)
   const [error, setError] = useState(null)
 
   const registerUser = async (e) => {
     e.preventDefault()
-    const newUser = await register('Jeeermaineee', 'shhhhhh')
+    // This is hard coded but you can write a form and create a user!
+    const newUser = await register('Bugsyeee', 'shhhhhh')
     if (newUser.name === 'error') {
       setError(newUser.detail)
     } else {
-      setUser(newUser)
+      setUser(newUser.user)
     }
   }
 
+  console.log('User', user)
   return (
     <div>
       {error ? <h3>Error! {error}</h3> : null}
       <button onClick={registerUser}>Register User</button>
+      {user ? <h3>Hello {user.username}</h3> : null}
     </div>
   )
 }
