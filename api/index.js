30fb8c88964d4b0abbacda4d231bc335417b8fdd
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-13 22:15:49 -0400

    api health

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/health', (req, res, next) => {
-  res.send('All healthy and ready to go!')
+  res.send('API is healthy and ready to go!')
 })
 router.use('/auth', require('./auth'))
 
